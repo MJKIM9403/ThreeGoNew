8b1702583d66eccdc0f03dd634a1069c947e5e8d
admin <kpark9700@gmail.com>
2024-01-25 16:44:42 +0900

    게시판 파일 첨부기능 구현 중...(잘않됨) planner 생성 중

---
+++ src/main/java/com/io/threegonew/domain/Planner.java
@@ -0,0 +1,39 @@
+package com.io.threegonew.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@NoArgsConstructor
+public class Planner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "planner_name", nullable = true)
+    private String plannerName;
+
+    @Column(name = "start_date", nullable = false)
+    private Date startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private Date endDate;
+
+    @Builder
+    public Planner(Date startDate, Date endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void update(String plannerName) {
+        this.plannerName = plannerName;
+    }
+}
---
+++ src/main/java/com/io/threegonew/repository/PlannerRepository.java
@@ -0,0 +1,7 @@
+package com.io.threegonew.repository;
+
+import com.io.threegonew.domain.Planner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlannerRepository extends JpaRepository<Planner, Integer> {
+}
