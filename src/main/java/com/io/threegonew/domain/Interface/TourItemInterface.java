324c23b518ee36cf3b669c80ce8cc7b37698e6e1
admin <kimkh134@gmail.com>
2024-02-02 16:42:18 +0900

    content 작성중...

---
+++ src/main/java/com/io/threegonew/domain/Interface/TourItemInterface.java
@@ -0,0 +1,8 @@
+package com.io.threegonew.domain.Interface;
+
+public interface TourItemInterface {
+    String getContentid();
+    String getFirstimage();
+    String getTitle();
+    Double getDistance();
+}
--- src/main/java/com/io/threegonew/repository/TourItemRepository.java
+++ src/main/java/com/io/threegonew/repository/TourItemRepository.java
@@ -1,12 +1,28 @@
 package com.io.threegonew.repository;
 
+import com.io.threegonew.domain.Interface.TourItemInterface;
 import com.io.threegonew.domain.TourItem;
+import com.io.threegonew.dto.TourItemResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TourItemRepository extends JpaRepository<TourItem, String>, JpaSpecificationExecutor<TourItem> {
     Page<TourItem> findAll(Specification spec, Pageable pageable);
+
+    @Query(value = "SELECT t.contentid, t.title, t.firstimage " +
+            "    ( 6371 * acos( cos( radians(:#{#response.mapy}) ) * cos( radians( t.mapy ) ) " +
+            "    * cos( radians( :#{#response.mapx} ) - radians(t.mapx) ) " +
+            "    + sin( radians(:#{#response.mapy}) ) * sin( radians( t.mapy ) ) ) ) AS distance " +
+            "FROM touritem t " +
+            "where cat2 = :#{#response.cat2} " +
+            "HAVING distance < 10 " +
+            "ORDER BY distance " +
+            "LIMIT 0 , 5;", nativeQuery = true)
+    List<TourItemInterface> findTourItemByDistanceAndCat2(TourItemResponse response);
 }
