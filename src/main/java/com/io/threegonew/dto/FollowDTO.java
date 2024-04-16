9bdeceec408d7fa558461d91e596861959e3f235
admin <kpark9700@gmail.com>
2024-04-16 14:30:50 +0900

    [수정] 유저가 나를 팔로우하는지 확인하는 코드 추가

--- src/main/java/com/io/threegonew/dto/FollowDTO.java
+++ src/main/java/com/io/threegonew/dto/FollowDTO.java
@@ -11,6 +11,7 @@ public class FollowDTO {
     private Long id;
     private UserInfoResponse toUser;
     private UserInfoResponse fromUser;
-    private int followState;
-    private int followCount;
+    private int followState; // 내가 팔로우 중인지
+    private int youFollowMeState; // 나를 팔로우 중인지
+    private int followCount; // 팔로우 중인 숫자
 }
