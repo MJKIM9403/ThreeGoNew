b2b10a037b297e6ea7352df62a5480e01b54d84e
admin <kimkh134@gmail.com>
2024-04-17 18:05:47 +0900

    검색 - 리뷰검색 추천순

--- src/main/java/com/io/threegonew/controller/SearchController.java
+++ src/main/java/com/io/threegonew/controller/SearchController.java
@@ -1,7 +1,10 @@
 package com.io.threegonew.controller;
 
+import com.io.threegonew.dto.UserInfoResponse;
+import com.io.threegonew.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/search")
 public class SearchController {
+    private final UserService userService;
     @GetMapping("")
-    public String getSearchInfo(){
+    public String getSearchInfo(Model model){
+        UserInfoResponse loginUserInfo;
+
+        try{
+            String loginUserId = userService.getCurrentUserId();
+            loginUserInfo = userService.findUserInfo(loginUserId);
+        }catch (IllegalArgumentException e){
+            loginUserInfo = UserInfoResponse.builder()
+                    .id("anonymousUser")
+                    .build();
+        }
+
+        model.addAttribute("loginUser", loginUserInfo);
         return "search/search";
     }
 }
