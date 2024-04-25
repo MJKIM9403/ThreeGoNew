c753805a752bc0cb6c195de88aa9adb3aa545c75
김재연 <kpark9700@gmail.com>
2024-04-26 03:38:06 +0900

    [수정] rest api 규칙에 따라 planner 관련 수정중

--- src/main/java/com/io/threegonew/controller/MyPlanApiController.java
+++ src/main/java/com/io/threegonew/controller/MyPlanApiController.java
@@ -37,49 +37,7 @@ public class MyPlanApiController {
         return "plan/plan :: #touritems";
     }
 
-    // 내가 북마크한 관광지를 cat2로 조회할 때
-    @GetMapping("/planner/bookmark/cat2")
-    public String getMyBookmarkCat2List(@ModelAttribute TourItemBookmarkRequest request, Model model) {
-
-        if(request.getCat1() != null){
-            model.addAttribute("cat2List", tourItemService.findCat2List(request.getCat1()));
-            model.addAttribute("cat3List", new ArrayList<>());
-        }else {
-            model.addAttribute("cat2List", new ArrayList<>());
-            model.addAttribute("cat3List", new ArrayList<>());
-        }
-
-        return "plan/plan :: #category-middle";
-    }
-
-    // 내가 북마크한 관광지를 시군구로 조회할 때
-    @GetMapping("/planner/bookmark/sigungu")
-    public String getMyBookmarkSigunguList(@ModelAttribute TourItemBookmarkRequest request, Model model) {
-
-        if(request.getAreaCode() != null){
-            model.addAttribute("sigunguList", tourItemService.findSigunguList(Integer.valueOf(request.getAreaCode())));
-
-        }else {
-            model.addAttribute("sigunguList", new ArrayList<>());
-        }
-
-        return "plan/plan :: #sigungu";
-    }
-
-    // 내가 북마크한 관광지를 cat3로 조회할 때
-    @GetMapping("/planner/bookmark/cat3")
-    public String getMyBookmarkCat3List(@ModelAttribute TourItemBookmarkRequest request, Model model){
-        if(request.getCat2() != null){
-            model.addAttribute("cat3List", tourItemService.findCat3List(request.getCat2()));
-        }else {
-            model.addAttribute("cat3List", new ArrayList<>());
-        }
-
-        return "plan/plan :: #category-row";
-    }
-
-
-    // 북마크 아닌, 전체 관광지를 여러 옵션으로 검색하여 조회할 때
+    // 북마크 아닌, 전체 관광지를 조회할 때
     @GetMapping("/planner/touritems")
     public String getTourItemList(@ModelAttribute TourItemSelectRequest request, Model model) {
         PageResponse pageResponse = tourItemService.findSelectedTourItemList(request);
@@ -87,7 +45,7 @@ public class MyPlanApiController {
         return "plan/plan :: #touritems";
     }
 
-    // 북마크 아닌, 전체 관광지를 ca2로 조회할 때
+    // cat1 선택시 cat2 목록 조회하기
     @GetMapping("/planner/cat2")
     public String getCat2List(@ModelAttribute TourItemSelectRequest request, Model model) {
 
@@ -102,7 +60,7 @@ public class MyPlanApiController {
         return "plan/plan :: #category-middle";
     }
 
-    // 북마크 아닌, 전체 관광지를 시군구로 조회할 때
+    // areaCode 선택 시 sigungu 목록 조회하기
     @GetMapping("/planner/sigungu")
     public String getSigunguList(@ModelAttribute TourItemSelectRequest request, Model model) {
 
@@ -115,7 +73,7 @@ public class MyPlanApiController {
         return "plan/plan :: #sigungu";
     }
 
-    // 북마크 아닌, 전체 관광지를 cat3로 조회할 때
+    // cat2 선택 시 cat3 목록 조회하기
     @GetMapping("/planner/cat3")
     public String getCat3List(@ModelAttribute TourItemSelectRequest request, Model model){
         if(request.getCat2() != null){
@@ -130,7 +88,7 @@ public class MyPlanApiController {
     /** 생성 메서드 **/
     // 플래너 생성
     @PostMapping(value = "/planner", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Long>> savePlan(@RequestBody CompletePlannerRequest request) {
+    public ResponseEntity<Map<String, Object>> savePlan(@RequestBody CompletePlannerRequest request) {
 
         String plannerName = request.getPlannerName();
         LocalDate startDate = request.getStartDate();
@@ -155,7 +113,7 @@ public class MyPlanApiController {
 
             if (plannerId == null) {
                 // plannerId가 null인 경우 처리
-                System.out.println("plannerId 반환 실패~~~");
+                System.out.println("plannerId 반환 실패");
             } else {
                 for(AddPlanRequest place: places) {
                     place.setUserId(userId);
@@ -164,10 +122,17 @@ public class MyPlanApiController {
                 }
             }
 
-            return ResponseEntity.ok().body(Collections.singletonMap("p_id", plannerId));
+            // 응답 데이터 생성
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("plannerId", plannerId);
+            responseData.put("plannerName", plannerName);
+            responseData.put("startDate", startDate);
+            responseData.put("endDate", endDate);
+            responseData.put("plans", places);
+            System.out.println(responseData.get("p_id").toString());
+            return ResponseEntity.ok().body(responseData);
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body(Collections.singletonMap("p_id", null));
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 저장에 실패했습니다.");
         }
     }
 
@@ -176,9 +141,9 @@ public class MyPlanApiController {
     public ResponseEntity<?> updatePlannerDates(@PathVariable("plannerId") Long plannerId, @RequestBody UpdatePlannerRequest request) {
         try {
             plannerService.updatePlannerDates(request.getPlannerId(), request.getStartDate(), request.getEndDate());
-            return ResponseEntity.ok("날짜가 성공적으로 업데이트 되었습니다!");
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("날짜 업데이트 실패: " + e.getMessage());
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 날짜 업데이트에 실패했습니다.");
         }
     }
 
@@ -186,15 +151,15 @@ public class MyPlanApiController {
     public ResponseEntity<?> updatePlannerName(@PathVariable Long plannerId, @RequestBody UpdatePlannerRequest request) {
         try {
             plannerService.updatePlannerName(plannerId, request.getPlannerName());
-            return ResponseEntity.ok().body("플래너 제목이 성공적으로 업데이트 되었습니다!");
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("플래너 제목 업데이트 실패 : " + e.getMessage());
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 제목 업데이트에 실패했습니다.");
         }
     }
 
 
     @PutMapping(value = "/planner/{plannerId}/plans", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Long>> updatePlan(@PathVariable("plannerId") Long plannerId, @RequestBody CompletePlannerRequest request) {
+    public ResponseEntity<Map<String, Object>> updatePlan(@PathVariable("plannerId") Long plannerId, @RequestBody CompletePlannerRequest request) {
 
         String plannerName = request.getPlannerName();
         LocalDate startDate = request.getStartDate();
@@ -216,10 +181,6 @@ public class MyPlanApiController {
             // 기존 계획을 삭제
             planService.deleteByPlannerId(plannerId); // 이 메서드도 필요에 따라 구현되어야 함
 
-            // 플래너를 업데이트하고 식별자를 반환
-            // Planner planner = plannerService.update(plannerId, plannerName, startDate, endDate, userId);
-
-
             for(AddPlanRequest place: places) {
                 place.setUserId(userId);
                 place.setPlannerId(plannerId);
@@ -227,30 +188,37 @@ public class MyPlanApiController {
             }
 
 
-            return ResponseEntity.ok().body(Collections.singletonMap("p_id", plannerId));
+            // 응답 데이터 생성
+            Map<String, Object> responseData = new HashMap<>();
+            responseData.put("plannerId", plannerId);
+            responseData.put("plannerName", plannerName);
+            responseData.put("startDate", startDate);
+            responseData.put("endDate", endDate);
+            responseData.put("plans", places);
+            return ResponseEntity.ok().body(responseData);
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.badRequest().body(Collections.singletonMap("p_id", null));
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 수정에 실패했습니다.");
         }
     }
 
     /** 삭제 메서드 **/
     // 플래너 삭제
     @DeleteMapping(value = "/planner/{plannerId}")
-    public ResponseEntity<String> updatePlannerDeleteFlag(@PathVariable Long plannerId) {
+    public ResponseEntity updatePlannerDeleteFlag(@PathVariable Long plannerId) {
         try {
             plannerService.updatePlannerDeleteFlag(plannerId);
-            return ResponseEntity.ok().body("플래너와 관련된 삭제 플래그가 성공적으로 수정되었습니다");
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.badRequest().body("플래너와 관련된 삭제 플래그를 수정하는 데 실패했습니다");
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 삭제에 실패했습니다.");
         }
     }
 
 
     /** 공유 관련 메서드 **/
     // 플랜 공유받았는지 확인하기
-    @GetMapping("/checkInvitation/{plannerId}/{guestId}")
+    @GetMapping("/planner/share/{plannerId}/{guestId}")
     public ResponseEntity<Map<String, Boolean>> checkInvitation(@PathVariable Long plannerId, @PathVariable String guestId) {
         boolean invited = teamService.isGuestAlreadyInvited(plannerId, guestId);
         Map<String, Boolean> response = new HashMap<>();
@@ -259,7 +227,7 @@ public class MyPlanApiController {
     }
 
     // 플래너 공유하기
-    @PostMapping("/sharePlanner")
+    @PostMapping("/planner/share")
     public ResponseEntity<?> sharePlanner(@RequestBody SharePlannerRequest request) {
         try {
             boolean isWriter = plannerService.isUserPlannerOwner(request.getOwnerId(), request.getPlannerId());
@@ -277,7 +245,7 @@ public class MyPlanApiController {
     }
 
     // 플래너에서 게스트 삭제하기
-    @DeleteMapping("/removeGuest/{plannerId}/{guestId}")
+    @DeleteMapping("/planner/share/{plannerId}/{guestId}")
     public ResponseEntity<?> removeGuestFromPlanner(@PathVariable Long plannerId, @PathVariable String guestId) {
         try {
             teamService.removeGuestFromPlanner(plannerId, guestId);
@@ -287,26 +255,20 @@ public class MyPlanApiController {
         }
     }
 
-
-
-    // 플랜을 날짜별로 가져와서 보여주기
-//    @PostMapping("/showplan")
-//    public ResponseEntity<List<PlanDTO<TourItemResponse>>> showPlan(@RequestBody PlanRequest request) {
-//        List<PlanDTO<TourItemResponse>> planDTOs = planService.findByPlannerIdAndDay(request);
-//        return new ResponseEntity<>(planDTOs, HttpStatus.OK);
-//    }
-
     /**
      * 조회 메서드
      */
     // 플랜 전체를 가져와서 보여주기
     @GetMapping("/planner/{plannerId}")
-    public ResponseEntity<List<PlanDTO<TourItemResponse>>> allShowPlan(@PathVariable Long plannerId, @ModelAttribute PlanRequest request) {
-        List<PlanDTO<TourItemResponse>> planDTOs = planService.findByPlannerId(request);
-        return new ResponseEntity<>(planDTOs, HttpStatus.OK);
+    public ResponseEntity allShowPlan(@PathVariable Long plannerId, @ModelAttribute PlanRequest request) {
+        try {
+            List<PlanDTO<TourItemResponse>> planDTOs = planService.findByPlannerId(request);
+            return ResponseEntity.ok().body(planDTOs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400", "플래너 조회에 실패했습니다.");
+        }
+
     }
-
-
-
 
 }
