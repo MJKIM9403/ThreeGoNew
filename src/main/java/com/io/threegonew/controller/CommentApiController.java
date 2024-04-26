d6f8fed380aac22e65bc17f5aa561e131e0047a3
김광호 <kimkh134@gmail.com>
2024-04-26 17:57:07 +0900

    댓글 api 수정

--- src/main/java/com/io/threegonew/controller/CommentApiController.java
+++ src/main/java/com/io/threegonew/controller/CommentApiController.java
@@ -6,74 +6,56 @@ import com.io.threegonew.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.AccessDeniedException;
+
 import java.util.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/comments")
+@RequestMapping("/api")
 @RestController
 public class CommentApiController {
     private final CommentService commentService;
 
-    @PostMapping("")
-    public ResponseEntity<Long> saveComment(@RequestBody AddCommentRequest request){
+    @PostMapping("/review/{reviewId}/comment")
+    public ResponseEntity saveComment(@RequestBody AddCommentRequest request){
         try{
             Comment savedComment = commentService.saveComment(request);
             return ResponseEntity.ok(savedComment.getCommentId());
-        }catch (IllegalArgumentException e){
+        }catch (AccessDeniedException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return ErrorResponse.createErrorResponse(HttpStatus.FORBIDDEN, "403", "댓글 작성 권한이 없습니다.");
         }
     }
-    @GetMapping("")
-    public ResponseEntity<PageResponse<CommentResponse>> showComments(@ModelAttribute CommentRequest request){
-        PageResponse<CommentResponse> pageResponse = commentService.getComments(request);
-        return ResponseEntity.ok().body(pageResponse);
-    }
-
-    @GetMapping("/replies")
-    public ResponseEntity<PageResponse<ReplyResponse>> showReplies(@ModelAttribute CommentRequest request){
-        PageResponse<ReplyResponse> pageResponse = commentService.getReplies(request);
-        return ResponseEntity.ok().body(pageResponse);
-    }
-
-    @GetMapping("/new-reply")
-    public ResponseEntity<Map<String,Object>> addNewReply(@RequestParam Long commentId){
-        try {
-            Comment reply = commentService.getComment(commentId);
-            ReplyResponse replyResponse = commentService.getReplyResponse(reply);
-            Long replyCount = commentService.getReplyCount(reply);
-
-            Map<String,Object> result = new HashMap<>();
-            result.put("reply", replyResponse);
-            result.put("replyCount", replyCount);
-
-            return ResponseEntity.ok().body(result);
-        }catch (IllegalArgumentException e){
+    @GetMapping("/review/{reviewId}/comments")
+    public ResponseEntity showComments(@PathVariable(value = "reviewId") Long reviewId,
+                                                                      @RequestParam(value = "page", defaultValue = "0") Integer pageNo)
+    {
+        try{
+            PageResponse<CommentResponse> pageResponse = commentService.getComments(reviewId, pageNo);
+            return ResponseEntity.ok().body(pageResponse);
+        }catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "댓글 목록 조회에 실패하였습니다.");
         }
     }
 
-    @GetMapping("/recent")
-    public ResponseEntity<List<CommentResponse>> showRecentComments(@RequestParam(value = "reviewId") Long reviewId,
-                                                                    @RequestParam(value = "lastCommentId") Long lastCommentId){
-        List<CommentResponse> recentComments = new ArrayList<>();
-        try {
-            if(commentService.hasNewComments(reviewId, lastCommentId)){
-                recentComments = commentService.getRecentComments(reviewId, lastCommentId);
-            }
-        }catch (IllegalArgumentException e){
+    @GetMapping("/review/{reviewId}/comment/{group}/replies")
+    public ResponseEntity showReplies(@PathVariable(value = "reviewId") Long reviewId,
+                                                                   @PathVariable(value = "group") Integer group,
+                                                                   @RequestParam(value = "page", defaultValue = "0") Integer pageNo)
+    {
+        try{
+            PageResponse<ReplyResponse> pageResponse = commentService.getReplies(reviewId, group, pageNo);
+            return ResponseEntity.ok().body(pageResponse);
+        }catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "답글 목록 조회에 실패하였습니다.");
         }
-        return ResponseEntity.ok().body(recentComments);
     }
 
-
-    @PutMapping("/{commentId}")
+    @PutMapping("/comment/{commentId}")
     public ResponseEntity<Map<String, Object>> updateComment(@RequestBody EditCommentRequest request) {
         try{
             Map<String, Object> result = new HashMap<>();
@@ -90,35 +72,72 @@ public class CommentApiController {
             return ResponseEntity.ok().body(result);
         }catch (IllegalArgumentException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "댓글 수정에 실패하였습니다.");
         }catch (AccessDeniedException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            return ErrorResponse.createErrorResponse(HttpStatus.FORBIDDEN, "403", "댓글 수정 권한이 없습니다.");
         }
     }
 
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping("/comment/{commentId}")
     public ResponseEntity deleteComment(@PathVariable(value = "commentId") Long commentId) {
         try{
             commentService.deleteComment(commentId);
             return new ResponseEntity<>(HttpStatus.OK);
         }catch (IllegalArgumentException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "댓글 삭제에 실패하였습니다.");
         }catch (AccessDeniedException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            return ErrorResponse.createErrorResponse(HttpStatus.FORBIDDEN, "403", "댓글 삭제 권한이 없습니다.");
         }
     }
 
-    @GetMapping("/count")
-    public ResponseEntity showCommentCount(@RequestParam(value = "reviewId") Long reviewId){
+    @GetMapping("/comment/{commentId}/temp-reply")
+    public ResponseEntity addTempReply(@PathVariable(value = "commentId") Long commentId){
+        try {
+            Comment reply = commentService.getComment(commentId);
+            ReplyResponse replyResponse = commentService.getReplyResponse(reply);
+            Long replyCount = commentService.getReplyCount(reply);
+
+            Map<String,Object> result = new HashMap<>();
+            result.put("reply", replyResponse);
+            result.put("replyCount", replyCount);
+
+            return ResponseEntity.ok().body(result);
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "답글 조회에 실패하였습니다.");
+        }
+    }
+
+    @GetMapping("/review/{reviewId}/comments/recent")
+    public ResponseEntity showRecentComments(@PathVariable(value = "reviewId") Long reviewId,
+                                             @RequestParam(value = "last") Long lastCommentId)
+    {
+        try {
+            List<CommentResponse> recentComments = new ArrayList<>();
+
+            if(commentService.hasNewComments(reviewId, lastCommentId)){
+                recentComments = commentService.getRecentComments(reviewId, lastCommentId);
+            }
+
+            return ResponseEntity.ok().body(recentComments);
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "최신 댓글 목록 조회에 실패하였습니다.");
+        }
+    }
+
+
+    @GetMapping("/review/{reviewId}/comments/count")
+    public ResponseEntity showCommentCount(@PathVariable(value = "reviewId") Long reviewId){
         try{
             Long allCommentsCount = commentService.getAllCommentsCount(reviewId);
             return ResponseEntity.ok().body(allCommentsCount);
         }catch (IllegalArgumentException e){
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ErrorResponse.createErrorResponse(HttpStatus.BAD_REQUEST, "400" , "총 댓글 수 조회에 실패하였습니다.");
         }
     }
 }
--- src/main/java/com/io/threegonew/dto/AddCommentRequest.java
+++ src/main/java/com/io/threegonew/dto/AddCommentRequest.java
@@ -1,5 +1,6 @@
 package com.io.threegonew.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
