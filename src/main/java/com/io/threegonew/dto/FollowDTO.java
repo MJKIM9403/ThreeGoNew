4a3c6ca8d0308f5b3a0dd7192452e36307a0fba0
김재연 <kpark9700@gmail.com>
2024-05-05 20:02:40 +0900

    [수정] 팔로우 관련 api 수정중

--- src/main/java/com/io/threegonew/dto/FollowDTO.java
+++ src/main/java/com/io/threegonew/dto/FollowDTO.java
@@ -15,6 +15,6 @@ public class FollowDTO {
     private String listSfile;
     private int followingState; // 내가 팔로우 중인지
     private int sameUserState; // 같은 유저인지
-    private int followedState; // 그 유저가 다를 팔로우 중인지
+    private int followedState; // 그 유저가 나를 팔로우 중인지
 
 }
