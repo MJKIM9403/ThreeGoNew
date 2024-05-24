92ae230bca4caa35b3fad013e0a286aa13386bb9
김광호 <kimkh134@gmail.com>
2024-05-24 15:44:25 +0900

    [변경] 테스트 제거(임시)

--- src/test/java/com/io/threegonew/ThreeGoNewApplicationTests.java
+++ src/test/java/com/io/threegonew/ThreeGoNewApplicationTests.java
@@ -3,11 +3,11 @@ package com.io.threegonew;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ThreeGoNewApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
